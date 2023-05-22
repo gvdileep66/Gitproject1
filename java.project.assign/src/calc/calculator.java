a41dfd8bd672ee271ad213dce2c5e4c06263adc1
gvdilip <gvdilipgvd.com>
2023-05-22 17:10:05 +0530

    commit the message

---
+++ java.project.assign/src/calc/calculator.java
@@ -0,0 +1,20 @@
+package calc;
+
+public class calculator {
+	public int sum (int a, int b) {
+		return (a + b);
+	}
+	public int minus (int a, int b){
+		return (a - b);///ERROR!!!
+	}
+	public int divide (int a, int b) {
+		return (a / b);
+	}
+	public int multipy (int a, int b) {
+		return (a * b);
+	}
+
+}
+
+
+
---
+++ java.project.assign/src/test/console/calculator.java
@@ -0,0 +1,22 @@
+package test.console;
+
+public class calculator {
+
+		public int sum(int a, int b) {
+			return (a + b);
+		}
+
+		public int minus(int a, int b) {
+			return (a - b);/// ERROR!!!
+		}
+
+		public int divide(int a, int b) {
+			return (a / b);
+		}
+
+		public int multipy(int a, int b) {
+			return (a * b);
+		}
+
+	}
+
